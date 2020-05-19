c189b9d1e18c425616cfcd864f84b7e74582d3d9
Shaun Chander <shaunchander@gmail.com>
2020-05-19 17:16:05 -0500

    ♻ make animation global

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
 import React from "react"
 
 // Component imports
-import Div100vh from "react-div-100vh"
+import Navbar from "./Navbar"
+import Div100Vh from "react-div-100vh"
+
+import { motion } from "framer-motion"
 
 interface Props {
   children: React.ReactNode
@@ -9,9 +12,23 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <main className="leading-relaxed font-jost text-gunmetalDark">
-      <Div100vh className="relative overflow-x-hidden">{children}</Div100vh>
-    </main>
+    <Div100Vh className="flex flex-col">
+      <header className="fixed inset-x-0 top-0 z-50">
+        <Navbar />
+      </header>
+      <motion.main
+        initial={{ scale: 1.2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 250,
+          damping: 100,
+        }}
+        className="flex-1 leading-relaxed font-jost text-gunmetalDark"
+      >
+        {children}
+      </motion.main>
+    </Div100Vh>
   )
 }
 
